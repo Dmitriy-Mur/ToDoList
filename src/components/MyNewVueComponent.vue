697a6d3323a0353f168cf43a86fcd6fd54f3cfcd
Dmitriy <d.a.muravyev1gmail.com>
2025-07-22 13:17:56 +0500

    Add counter

--- src/components/MyNewVueComponent.vue
+++ src/components/MyNewVueComponent.vue
@@ -1,4 +1,25 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const text = ref<string>('Its work!')
+const count = ref(0)
+
+const increment = () => {
+  count.value++
+}
+
+alert(text.value)
+</script>
+
 <template>
   <p>This is Vue component</p>
-  <button>Click</button>
+  <p>tasks count:</p>
+  <h1>{{ count }}</h1>
+  <button @click="increment">Click me</button>
 </template>
+
+<style scoped>
+p {
+  font-family: 'Courier New', Courier, monospace;
+}
+</style>
